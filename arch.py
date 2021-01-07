bff5d1ff3c9beacbbae0f1c4fefcaa1c711c31f1
mobalk <mobalkhu@gmail.com>
2021-01-07 22:56:13 +0100

    arch.py: archive only older files
    treshold is 7 days
    Also, use a separate directory where the archived images are moved
    (for later deletion).

--- arch.py
+++ arch.py
@@ -2,19 +2,16 @@ import configparser
 import os
 from PIL import Image, ImageChops, ImageEnhance
 import logging
+import time
 
-LOGFILE = "arch.log"
+SEC_PER_DAY = 60 * 60 * 24
 
 logging.basicConfig(level=logging.DEBUG)
-#logging.basicConfig(filename=LOGFILE, level=logging.DEBUG)
-print("Logging to " + LOGFILE)
-
+dryRun = False
 
 config = configparser.ConfigParser()
 config.read('config.ini')
 
-#newsize = (1014, 760) # 50 % of 2028 x 1560
-#newsize = (811, 608) # 40% of 2028 x 1560
 factor = 0.4
 
 
@@ -32,23 +29,30 @@ except FileExistsError:
 im1 = None
 im2 = None
 ans = None
+archiveUntil = time.time() - 7 * SEC_PER_DAY
 
 for filename in sorted(os.listdir(directory)):
-    logging.debug(directory + " / " + filename)
-    #userinput = input("go?")
-    im1 = Image.open(directory + "/" + filename)
-    newsize = (int(im1.width * factor), int(im1.height * factor))
-    im1 = im1.resize(newsize, resample=Image.LANCZOS)
-    newPath = directory + "/arch/" + filename
-    if os.path.exists(newPath) and ans != "O":
-        if ans == "S": continue
-        print(newPath + " exists. What to do?\n [o]verwrite, [s]kip, [O]verwrite all, [S]kip all, e[x]it? ")
-        while True:
-            ans = input()
-            if ans in ('o', 'O', 's', 'S', 'x'): break
-        if ins == "s": continue
-        if ans == "x": break
+    fPath = os.path.join(directory, filename)
+    newPath = os.path.join(directory, '/arch/', filename)
+    movePath = os.path.join(directory, '/archived/', filename)
+    logging.debug(fPath)
 
-    im1.save(directory + "/arch/" + filename, quality=95, optimize = True)
-    os.replace(directory + "/" + filename, directory + "/archived/" + filename)
+    if os.path.isfile(fPath) and os.stat(fPath).st_mtime < archiveUntil:
+        im1 = Image.open(fPath)
+        newsize = (int(im1.width * factor), int(im1.height * factor))
+        im1 = im1.resize(newsize, resample = Image.LANCZOS)
+        if os.path.exists(newPath) and ans != "O":
+            if ans == "S": continue
+            print(newPath + " exists. What to do?")
+            print("[o]verwrite, [s]kip, [O]verwrite all, [S]kip all, e[x]it? ")
+            while True:
+                ans = input()
+                if ans in ('o', 'O', 's', 'S', 'x'): break
+                if ins == "s": continue
+                if ans == "x": break
 
+        if dryRun:
+            logging.info('Save ' + newPath)
+        else:
+            im1.save(newPath, quality=80, optimize = True)
+            os.replace(fPath, movePath)
