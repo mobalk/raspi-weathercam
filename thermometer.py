8dca36a523cac7b0a5aef1011279ce22573d57ab
mobalk <mobalkhu@gmail.com>
2021-02-22 22:58:48 +0100

    thermometer: implement remote power on/off
    DHT22 sensor stops working after a while. In such cases turning it off
    then on again usually helps.
    If the DHT sensor is powered from a GPIO pin, the software can handle
    the power off/on switches.

--- thermometer.py
+++ thermometer.py
@@ -6,17 +6,41 @@ import adafruit_dht
 import sqlite3
 import config
 import logging
+import gpiozero
+from gpiozero.pins.rpigpio import RPiGPIOFactory
 
 LOGFILE = "temperature.log"
 
 logging.basicConfig(filename=LOGFILE, level=logging.INFO)
+#logging.basicConfig(level=logging.INFO)
 print("Logging to " + LOGFILE)
 
+def get_power_pin(config):
+    powerPin = config.getint('thermo', 'PowerPin', fallback=-1)
+    if powerPin != -1:
+        return gpiozero.pins.rpigpio.RPiGPIOPin(RPiGPIOFactory(),22)
+    else:
+        return None
 
-# Initial the dht device, with data pin connected to:
-dhtDevice = adafruit_dht.DHT22(board.D4, use_pulseio=False)
+def power_switch(pin, state):
+    if pin:
+        pin.output_with_state(state)
+
+#  hello, IT
+def have_you_tried_turning_it_off_and_on_again(pin):
+    logging.warning("Turn OFF then turn ON the DHT sensor")
+    power_switch(pin, 0)
+    time.sleep(3)
+    power_switch(pin, 1)
 
 conf = config.init()
+
+powerPin = get_power_pin(conf)
+power_switch(powerPin, 1)
+
+# Initiate the dht device, with data pin connected to:
+dhtDevice = adafruit_dht.DHT22(board.D4, use_pulseio=False)
+
 dbPath = conf.get('app', 'PathToDatabase')
 
 conn = sqlite3.connect(dbPath)
@@ -31,6 +55,7 @@ with conn:
 
     # Default sleep before retry in case of exception. After continous errors we'll increase that.
     sleepException = 3.0
+    sensor_not_found = 0
     while True:
         try:
             # Print the values to the serial port
@@ -46,6 +71,12 @@ with conn:
             logging.warning(time.strftime("%Y-%m-%d %H:%M:%S ") + error.args[0])
             time.sleep(sleepException)
             sleepException *= 2
+            if "DHT sensor not found" in error.args[0]:
+                sensor_not_found += 1
+                if sensor_not_found == 3:
+                    have_you_tried_turning_it_off_and_on_again(powerPin)
+            else:
+                sensor_not_found = 0
             continue
         except Exception as error:
             dhtDevice.exit()
@@ -53,3 +84,4 @@ with conn:
 
         time.sleep(45)
         sleepException = 3.0
+        sensor_not_found = 0
