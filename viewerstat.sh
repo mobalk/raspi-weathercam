9affacfeb4d671095ba106cf02571a4b546c2c71
mobalk <mobalkhu@gmail.com>
2021-03-13 01:04:27 +0100

    viewerstat: send alert message in email
    recognize if camera doesn't upload for a while and send email in such
    case

--- viewerstat.sh
+++ viewerstat.sh
@@ -2,9 +2,30 @@
 SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 cd $SCRIPTPATH
 
-FileName=`mktemp`
-UserName=$(python3 config.py upload User) # how cool is this easy config read...
-PrivateDir=$(python3 config.py app PrivateDir)
-curl -s -o $FileName "https://www.idokep.hu/webkamera/$UserName"
-Viewers=$(grep "látogató az elmúlt 5 percben" $FileName | cut -d',' -f2 | sed -e 's/.*>\([0-9]\+\).*/\1/')
-echo "$(date +'%Y-%m-%d %H:%M'), $Viewers" >> ${PrivateDir}/viewerstat.csv
+if [ -z "$1" ]
+then
+	FileName=`mktemp`
+	UserName=$(python3 config.py upload User) # how cool is this easy config read...
+	curl -s -o $FileName "https://www.idokep.hu/webkamera/$UserName"
+else
+	FileName="$1"
+fi
+
+Viewers=$(grep "látogató az elmúlt 5 percben" $FileName | cut -d',' -f2 | sed -e 's/.* \([0-9]\+\).*/\1/')
+
+if [ -z "$Viewers" ]
+then
+	AlertMsg=$(grep "Utolsó képkocka" $FileName | sed -e 's/.*p>\(.*\)<\/p.*/\1/')
+	if [ -n "AlertMsg" ]
+	then
+		#echo "$AlertMsg"
+		if command -v msmtp &> /dev/null
+		then
+			echo "Subject: $AlertMsg" | msmtp $(python3 config.py email Address)
+		fi
+	fi
+else
+	PrivateDir=$(python3 config.py app PrivateDir)
+	# echo "$(date +'%Y-%m-%d %H:%M'), $Viewers"
+	echo "$(date +'%Y-%m-%d %H:%M'), $Viewers" >> ${PrivateDir}/viewerstat.csv
+fi
